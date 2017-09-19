395b011eb932ce61f8fbce60599566b1344edd43
Shawn <steel4234@yahoo.com>
2017-09-19 18:50:26 -0400

    push test

--- Tester.java
+++ Tester.java
@@ -1,7 +1,8 @@
 public class Tester {
     public static void main(String[] args)
     {
-        Patient test = new Patient("Shawn", "Harrington", 999);
+        Patient test = new Patient("Shawn", "Harrington", 999, 0);
         System.out.println(test.getTime());
+        System.out.println("Push test");
     }
 }
